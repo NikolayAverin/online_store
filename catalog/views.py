6294a97506dc4a115b9bfbe08c8c22b756f67cf6
Nikolay Averin <rizzzon116@mail.ru>
2024-04-11 22:20:23 +0400

    finalized template

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from configurations import APPEALS
+import json
 
 
 def home_page(request):
@@ -6,4 +8,12 @@ def home_page(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        data_to_write = {"name": name, "phone": phone, "message": message}
+        print(f'{name} просит связаться по номеру {phone}. Его вопрос: {message}')
+        with open(APPEALS, "a") as file:
+            json.dump(data_to_write, file, indent=4)
     return render(request, 'catalog/contacts.html')
