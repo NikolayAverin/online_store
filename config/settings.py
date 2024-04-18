a553683c9941a29c662830eb448b7f7c13c9296f
Nikolay Averin <rizzzon116@mail.ru>
2024-04-18 08:27:46 +0400

    add connect to db

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'off_road_store',
+        'USER': 'postgres',
+        'PASSWORD': '111111'
     }
 }
 
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
