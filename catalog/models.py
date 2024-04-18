d7d9b4e4ca3f787283b33bf39046fa0fcc4e604f
Nikolay Averin <rizzzon116@mail.ru>
2024-04-18 16:57:31 +0400

    add model product and category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    price = models.IntegerField(verbose_name='Цена')
+    photo = models.ImageField(upload_to='catalog/', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name} - {self.price} руб'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
