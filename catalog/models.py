42385a6ae8f393c09b6170b9791b0c4a229dc254
Nikolay Averin <rizzzon116@mail.ru>
2024-04-18 17:09:59 +0400

    edit models, settings, urls

--- catalog/models.py
+++ catalog/models.py
@@ -3,20 +3,6 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Название')
-    description = models.TextField(verbose_name='Описание')
-    price = models.IntegerField(verbose_name='Цена')
-    photo = models.ImageField(upload_to='catalog/', **NULLABLE)
-
-    def __str__(self):
-        return f'{self.name} - {self.price} руб'
-
-    class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
@@ -27,3 +13,22 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    price = models.IntegerField(verbose_name='Цена')
+    photo = models.ImageField(upload_to='catalog/', **NULLABLE)
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_atзщ = models.DateField(verbose_name='Дата последнего изменения')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+
+    def __str__(self):
+        return f'{self.name} - {self.price} руб'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
